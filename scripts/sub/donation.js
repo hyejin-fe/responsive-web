97c7d998b448b02b8e3582b009b5612b16b71371
hyejin <ryuheyjin0415@gmail.com>
2026-05-11 11:51:11 +0900

    jin 도서기증 스크립트 시작

---
+++ scripts/sub/donation.js
@@ -0,0 +1,85 @@
+/* 선택자 */
+const tabBtn = document.querySelector(".tab-btn");
+const infoBtn = tabBtn.querySelector(".info-btn");
+const applyBtn = tabBtn.querySelector(".apply-btn");
+const inquiryBtn = tabBtn.querySelector(".inquiry-btn");
+const listBtn = tabBtn.querySelector(".list-btn");
+
+const tabCont = document.querySelector(".tab-cont");
+const donaInfo = tabCont.querySelector(".dona-info");
+const donaApply = tabCont.querySelector(".dona-apply");
+const donaInquiry = tabCont.querySelector(".dona-inquiry");
+const donaList = tabCont.querySelector(".dona-list");
+
+const libraryCh = donaApply.querySelector(".library-ch");
+const tellNum = donaApply.querySelector(".tell-num");
+
+// 1. .tab-btn button 클릭시, 클릭한 버튼에 클래스 추가
+infoBtn.addEventListener("click", ()=>{
+    switchTab(infoBtn, donaInfo);
+});
+
+applyBtn.addEventListener("click", ()=>{
+    switchTab(applyBtn, donaApply);
+});
+
+inquiryBtn.addEventListener("click", ()=>{
+    switchTab(inquiryBtn, donaInquiry);
+});
+
+listBtn.addEventListener("click", ()=>{
+    switchTab(listBtn, donaList);
+});
+
+
+// 2. dona-apply 中 기증도서관선택 : li클릭시, ul에 show 클래스 토글 + 클릭한 li에 active클랙스 추가
+libraryCh.addEventListener("click", (e)=>{
+    dropdown(e);
+});
+
+tellNum.addEventListener("click", (e)=>{
+    dropdown(e);
+});
+
+
+// 1. 텝버튼 함수
+function switchTab(btn, cont){
+    btn.classList.add("active");
+    [...btn.parentElement.children]
+        .filter(function(child){
+            return child != btn;
+        })
+        .forEach(function(chlid){
+            chlid.classList.remove("active");
+        })
+
+    cont.classList.add("show");
+    [...cont.parentElement.children]
+        .filter(function(child){
+            return child != cont;
+        })
+        .forEach(function(chlid){
+            chlid.classList.remove("show");
+        })
+
+    tabBtn.classList.toggle("show");
+}
+
+// 2. 드롭다운 함수
+function dropdown(e){
+    const clickLi = e.target.closest("li");
+    if(!clickLi) return;
+
+    const ul = clickLi.parentElement;
+
+    clickLi.classList.add("active");
+    [...ul.children]
+        .filter(function(child){
+            return child != clickLi;
+        })
+        .forEach(function(child){
+            child.classList.remove("active");
+        })
+
+    ul.classList.toggle("show");
+}
