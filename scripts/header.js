e6ef8381ac66e84a40452d39fe14c1812666d3dc
hyejin <ryuheyjin0415@gmail.com>
2026-05-11 15:00:51 +0900

    jin 헤더 업데이트

--- scripts/header.js
+++ scripts/header.js
@@ -12,6 +12,8 @@ const searchClose = searchArea.querySelector("button");
 const gnb = document.querySelector("#gnb");
 const closeBtn = gnb.querySelector(".close-btn");
 
+const gnbTabs = gnb.querySelectorAll(".gnb-tab");
+
 
 // 0. 페이지 로드시, gnb 함수 실행
 gnbBaseChange();
@@ -42,6 +44,20 @@ closeBtn.addEventListener("click", ()=>{
     gnb.classList.remove("show");
 });
 
+
+// 4. 데스크탑버전 - 
+gnbTabs.forEach(function(tab){
+    const menuTit = tab.querySelector(".menu-tit");
+    const subMenu = tab.querySelector(".sub-menu");
+
+    subMenu.addEventListener('mouseenter', ()=>{  // mouseenter : 마우스가 요소 안으로 들어올 때
+        menuTit.classList.add('active');
+    });
+
+    subMenu.addEventListener('mouseleave', ()=>{  // mouseenter : 마우스가 요소 밖으로 나갈 때
+        menuTit.classList.remove('active');
+    });
+});
 
 
 // 0. gnb 함수 : 너비에 따라 클래스 변경
