7e17ac2f96cba2fc19dc04425ccc6049b0e2a7bd
hyejin <ryuheyjin0415@gmail.com>
2026-05-18 11:52:14 +0900

    jin 헤더 및 메인 수정완료

--- scripts/header.js
+++ scripts/header.js
@@ -11,7 +11,6 @@ const searchClose = searchArea.querySelector("button");
 
 const gnb = document.querySelector("#gnb");
 const closeBtn = gnb.querySelector(".close-btn");
-
 const gnbTabs = gnb.querySelectorAll(".gnb-tab");
 
 
@@ -27,7 +26,7 @@ snsBtn.addEventListener("click", ()=>{
 });
 
 
-// 2. 모바일버전 - 버튼 클릭시, searchArea에 show클래스 생성/제거
+// 2-1. 모바일버전 - 검색 버튼 클릭시, searchArea에 show클래스 생성/제거
 searchBtn.addEventListener("click", ()=>{
     searchArea.classList.add("show");
 });
@@ -36,13 +35,55 @@ searchClose.addEventListener("click", ()=>{
 });
 
 
-// 3. 모바일버전 - 버튼 클릭시, gnb에 show클래스 생성/제거
+// 2-2. 모바일버전 - 메뉴 버튼 클릭시, gnb에 show클래스 생성/제거
 menuBtn.addEventListener("click", ()=>{
     gnb.classList.add("show");
 });
 closeBtn.addEventListener("click", ()=>{
     gnb.classList.remove("show");
 });
+
+
+// 3. 모바일버전 - li(.gnb-tab) 클릭시, li에 active클래스 생성/제거
+gnbTabs.forEach(function(tab){
+    const menuTit = tab.querySelector(".menu-tit");
+
+    menuTit.addEventListener("click", ()=>{
+        if(!gnb.classList.contains("m-base")) return;
+
+        gnbTabs.forEach(function(t){
+            t.classList.remove("show")
+        });
+        tab.classList.add("show");
+    });
+
+    // 3-1. 모바일버전 - sub-menu > li > a 클릭시, depth3에 on클래스 생성/제거
+    const subMenu = tab.querySelector(".sub-menu");
+    const subTabs = subMenu.querySelectorAll(".sub-tab");
+    subTabs.forEach(function(li){
+        li.addEventListener("click", (e)=>{
+            e.stopPropagation(); // 탭 클릭 이벤트 버블링 방지 : 이 이벤트는 여기서 끝내고 위로 올려보내지 마
+        
+            const depth3 = li.querySelector(".depth3");
+            if(depth3){
+                const isOpen = depth3.classList.contains("on"); // 클릭 전 열림 상태 저장
+                
+                subTabs.forEach(function(t){
+                    t.querySelector(".depth3")?.classList.remove("on"); // 모두 닫기
+                });
+
+                if(!isOpen){ // 닫혀있었으면 열기
+                    depth3.classList.add("on");
+                }
+            }
+        });
+    });
+});
+
+
+// subTabs.forEach(function(t){
+//                     t.querySelector(".depth3")?.classList.remove("on")  // ? : 옵셔널 체이닝 - 앞의 값이 null/undefined면 실행하지 말고 그냥 넘어가
+//                 });
 
 
 
